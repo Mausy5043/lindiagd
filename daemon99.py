d0065f2a0db40ecc0eb2d2a5fe6c81edb2662ff4
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-06-11 16:17:23 +0200

    only need 10 lines from `/proc/mdstat`

--- daemon99.py
+++ daemon99.py
@@ -108,7 +108,7 @@ def do_xml(wpath):
 	uptime          = commands.getoutput("uptime")
 	dfh             = commands.getoutput("df")
 	freeh           = commands.getoutput("free")
-	mds							= commands.getoutput("cat /proc/mdstat |awk 'NR<12'")
+	mds							= commands.getoutput("cat /proc/mdstat |awk 'NR<10'")
 	psout           = commands.getoutput("top -b -n 1 | cut -c 37- | awk 'NR>4' | head -10 | sed 's/&/\&amp;/g' | sed 's/>/\&gt;/g'")
 	#Tsda            = commands.getoutput("smartctl -A /dev/sda -d ata |grep Temperature_Celsius |awk '{print $10}'")
 	#Tsdb            = commands.getoutput("smartctl -A /dev/sdb -d ata |grep Temperature_Celsius |awk '{print $10}'")
