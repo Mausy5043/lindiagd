9abf6a27b787d1d7b43b56a589ffab465a89a460
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-01 11:20:45 +0100

    add error logging and cycles

--- daemon12.py
+++ daemon12.py
@@ -8,6 +8,7 @@
 
 # daemon12.py measures the CPU load.
 
+import syslog, traceback
 import os, sys, time, math, commands
 from libdaemon import Daemon
 
@@ -16,9 +17,12 @@ DEBUG = False
 class MyDaemon(Daemon):
   def run(self):
     sampleptr = 0
-    samples = 5
+    cycles = 3
+    SamplesPerCycle = 5
+    samples = SamplesPerCycle * cycles
+
     datapoints = 11
-    data = [[None]*datapoints for _ in range(samples)]
+    data = []      # data = [None for i in range(samples)]
 
     sampleTime = 12
     cycleTime = samples * sampleTime
@@ -27,29 +31,48 @@ class MyDaemon(Daemon):
     if DEBUG:print "Waiting {0} s".format(int(waitTime))
     time.sleep(waitTime)
     while True:
-      startTime = time.time()
+      try:
+        startTime = time.time()
+        result = do_work().split(',')
+        if DEBUG: print result
 
-      result = do_work().split(',')
-      if DEBUG: print result
+        data.append(map(float, result))
+        if (len(data) > samples):data.pop(0)
+        sampleptr = sampleptr + 1
 
-      data[sampleptr] = map(float, result)
-      # report sample average
-      sampleptr = sampleptr + 1
-      if (sampleptr == samples):
-        somma = map(sum,zip(*data))
-        # not all entries should be float
-        # 0.37, 0.18, 0.17, 4, 143, 32147, 3, 4, 93, 0, 0
-        averages = [format(s / samples, '.3f') for s in somma]
-        averages[3]=int(data[sampleptr-1][3])
-        averages[4]=int(data[sampleptr-1][4])
-        averages[5]=int(data[sampleptr-1][5])
-        do_report(averages)
-        sampleptr = 0
+        # report sample average
+        if (sampleptr % SamplesPerCycle == 0):
+          if DEBUG:print data
+          somma = map(sum,zip(*data))
+          # not all entries should be float
+          # 0.37, 0.18, 0.17, 4, 143, 32147, 3, 4, 93, 0, 0
+          averages = [format(s / len(data), '.3f') for s in somma]
+          averages[3]=int(data[sampleptr-1][3])
+          averages[4]=int(data[sampleptr-1][4])
+          averages[5]=int(data[sampleptr-1][5])
+          if DEBUG:print "average:", averages
+          do_report(averages)
+          if (sampleptr == samples):
+            sampleptr = 0
 
-      waitTime = sampleTime - (time.time() - startTime) - (startTime%sampleTime)
-      if (waitTime > 0):
-        if DEBUG:print "Waiting {0} s".format(int(waitTime))
-        time.sleep(waitTime)
+        waitTime = sampleTime - (time.time() - startTime) - (startTime%sampleTime)
+        if (waitTime > 0):
+          if DEBUG:print "Waiting {0} s".format(int(waitTime))
+          time.sleep(waitTime)
+      except Exception as e:
+        if DEBUG:
+          print("Unexpected error:")
+          print e.message
+        syslog.syslog(syslog.LOG_ALERT,e.__doc__)
+        syslog_trace(traceback.format_exc())
+        raise
+        
+def syslog_trace(trace):
+  '''Log a python stack trace to syslog'''
+  log_lines = trace.split('\n')
+  for line in log_lines:
+    if len(line):
+      syslog.syslog(syslog.LOG_ALERT,line)
 
 def do_work():
   # 6 datapoints gathered here
